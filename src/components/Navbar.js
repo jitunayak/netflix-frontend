594c467be5af1a78b5abbda5326dbcca9fb6312e
Nayak <jitu.nayak@kochgs.com>
2021-12-12 00:59:48 +0530

    added cover image

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import MovieList from "./MovieList";
 
 export default function Navbar() {
   return (
@@ -7,6 +8,12 @@ export default function Navbar() {
         <h1 className="text-3xl font-bold text-red-100">Netflix</h1>
         <div className="text-white  self-center">Login</div>
       </div>
+      <img
+        className="w-full h-96 object-center"
+        src="https://images.thequint.com/thequint%2F2021-09%2Fb9aab177-f368-4185-91b4-b37f15c2b7ee%2Fmoneyheist.png?w=1200&auto=format%2Ccompress&ogImage=true"
+      />
+      <MovieList />
+      <MovieList />
     </div>
   );
 }
