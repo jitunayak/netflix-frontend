b924e7fcd7c90f13be5a5bfc6828e32c2dd13251
Nayak <jitu.nayak@kochgs.com>
2021-12-14 11:49:42 +0530

    proper handling of auth flow from cognito to application

--- src/App.js
+++ src/App.js
@@ -3,11 +3,60 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import MovieList from "./components/MovieList";
 import ScrollImages from "./components/ScrollImages";
+import { useState, useEffect } from "react";
+import { isExpired, decodeToken } from "react-jwt";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const loginUrl =
+    "https://netflix.auth.ap-south-1.amazoncognito.com/login?client_id=2v4q07qaad1jlugonm96akg1nk&response_type=token&scope=email+openid&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fcallback";
+
+  function setStoredToken(userToken) {
+    sessionStorage.setItem("token", JSON.stringify(userToken));
+  }
+  function getStoredToken() {
+    const tokenString = sessionStorage.getItem("token");
+    if (tokenString == null || tokenString == undefined) {
+      return null;
+    }
+    const userToken = JSON.parse(tokenString);
+    return userToken;
+  }
+
+  function getTokenFromCallbackUrl() {
+    try {
+      const pathname = window.location.href.split("=");
+      const myDecodedToken = decodeToken(pathname[1]);
+      setStoredToken(myDecodedToken);
+      return myDecodedToken;
+    } catch (err) {
+      return null;
+    }
+  }
+
+  useEffect(() => {
+    // if (isExpired(getToken())) {
+    //   window.open(loginUrl, "_self");
+    // }
+
+    //https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/loading-browser-credentials-cognito.html
+
+    if (getStoredToken() === null && getTokenFromCallbackUrl() === null) {
+      window.open(loginUrl, "_self");
+      console.log("token is null");
+      setUsername("No User was found");
+    } else {
+      setUsername(getStoredToken().email);
+    }
+
+    return () => {};
+  }, []);
+
   return (
     <div className="bg-black">
+      <h1 className="text-white text-lg">Hello, {username}</h1>
       <Navbar />
+      <MovieList />
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import MovieList from "./MovieList";
 
 export default function Navbar() {
   const loginUrl =
-    "https://netflix.auth.ap-south-1.amazoncognito.com/login?client_id=2v4q07qaad1jlugonm96akg1nk&response_type=token&scope=email+openid&redirect_uri=https%3A%2F%2Fnetflix-jitunayak.vercel.app%2Fcallback";
+    "https://netflix.auth.ap-south-1.amazoncognito.com/login?client_id=2v4q07qaad1jlugonm96akg1nk&response_type=token&scope=email+openid&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fcallback";
   return (
     <div>
       <div className="bg-red-700 p-2 justify-between flex flex-row">
@@ -16,11 +16,13 @@ export default function Navbar() {
         </button>
       </div>
       <img
+        className="w-64"
+        src="https://netflix-videos.s3.ap-south-1.amazonaws.com/Netflix-Logo.png"
+      />
+      <img
         className="w-full h-96 object-center"
         src="https://images.thequint.com/thequint%2F2021-09%2Fb9aab177-f368-4185-91b4-b37f15c2b7ee%2Fmoneyheist.png"
       />
-      <MovieList />
-      <MovieList />
     </div>
   );
 }
