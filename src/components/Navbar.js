d9c9cad71a50db6372a67f1fa4d82ba1e13743df
Nayak <jitu.nayak@kochgs.com>
2021-12-14 15:49:14 +0530

    fixed login button redirecting to hardcoded localhost

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,10 @@ import React from "react";
 import MovieList from "./MovieList";
 
 export default function Navbar() {
-  const loginUrl =
-    "https://netflix.auth.ap-south-1.amazoncognito.com/login?client_id=2v4q07qaad1jlugonm96akg1nk&response_type=token&scope=email+openid&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fcallback";
+  const domain = window.location.href;
+  const loginUrl = `https://netflix.auth.ap-south-1.amazoncognito.com/login?client_id=2v4q07qaad1jlugonm96akg1nk&response_type=token&scope=email+openid&redirect_uri=${encodeURIComponent(
+    domain
+  )}callback`;
   return (
     <div>
       <div className="bg-red-700 p-2 justify-between flex flex-row">
