e2217b1e2b08ad4fc9d4cb5c8d04f83da7cd2ffc
jitunayak <jitunayak715@gmail.com>
2022-04-30 01:24:12 +0530

    added login field validation

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -1,6 +1,39 @@
+import { useState } from "react";
+
 export default function LoginScreen() {
   const NETFLIX_URL =
     "https://assets.brand.microsites.netflix.io/assets/7dc497e2-4975-11ec-a9ce-066b49664af6_cm_1440w.jpg?v=1";
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const validateEmail = (email) => {
+    if (email.length === 0) {
+      return true;
+    }
+    if (
+      (email.length > 6 &&
+        email.includes("@") &&
+        email.includes(".") &&
+        (email.includes("com") ||
+          email.includes("net") ||
+          email.includes("org"))) ||
+      email.includes("in")
+    ) {
+      return true;
+    }
+    return false;
+  };
+
+  const validatePassword = (password) => {
+    if (password.length === 0) {
+      return true;
+    }
+    if (password.length >= 8) {
+      return true;
+    }
+    return false;
+  };
 
   return (
     <div className="justify-center flex">
@@ -14,15 +47,32 @@ export default function LoginScreen() {
           <div className="text-white font-extrabold text-2xl">Sign In</div>
           <input
             className="bg-zinc-700 rounded-sm py-3 px-4  mt-6 text-base text-white "
-            type="text"
+            type="email"
+            inputMode="email"
             placeholder="Email or phone number"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
           />
+          {!validateEmail(username) ? (
+            <label className="text-red-400  text-sm py-1">
+              Enter a valid email or phone number
+            </label>
+          ) : null}
+
           <input
-            className="bg-zinc-700 rounded-sm py-3 px-4  mt-4 text-base"
+            className="bg-zinc-700 rounded-sm py-3 px-4  mt-4 text-base text-white"
             type="password"
             placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
-          <button className="bg-red-600 rounded-sm py-2 px-2 mt-10 text-white font-bold text-lg ">
+          {!validatePassword(password) ? (
+            <label className="text-red-400  text-sm py-1">
+              Minimum length should be 6 characters
+            </label>
+          ) : null}
+
+          <button className="bg-red-600 hover:bg-red-700 rounded-sm py-2 px-2 mt-10 text-white font-bold text-lg ">
             Sign In
           </button>
           <div className="flex-row flex py-10">
