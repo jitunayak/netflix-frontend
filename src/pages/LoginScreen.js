5356b810faf16aab9969e7e05d1689b945365242
jitunayak <jitunayak715@gmail.com>
2022-05-21 04:56:42 +0530

    added bg cover in login screen

--- src/pages/LoginScreen.js
+++ src/pages/LoginScreen.js
@@ -6,7 +6,7 @@ import Loader from "../components/Loader";
 
 export default function LoginScreen() {
   const NETFLIX_URL =
-    "https://assets.brand.microsites.netflix.io/assets/7dc497e2-4975-11ec-a9ce-066b49664af6_cm_1440w.jpg?v=1";
+    "http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png";
 
   const navigate = useNavigate();
   const [username, setUsername] = useState("");
@@ -55,65 +55,75 @@ export default function LoginScreen() {
   };
 
   return (
-    <form>
-      <div className="flex justify-center items-center h-screen">
-        <div className="flex-col flex rounded-sm">
-          <img
-            alt="netflix logo"
-            src={NETFLIX_URL}
-            className="w-60 h-36 self-center"
-          ></img>
-          <div className="flex-col flex w-96 bg-zinc-900 p-8 rounded-sm">
-            <div className="text-white font-extrabold text-2xl">Sign In</div>
-            <input
-              className="bg-zinc-700 rounded-sm py-2 px-4  mt-6 text-base text-white "
-              type="email"
-              inputMode="email"
-              placeholder="Email or phone number"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            {!validateEmail(username) ? (
-              <label className="text-red-400  text-sm py-1">
-                Enter a valid email or phone number
-              </label>
-            ) : null}
-            <input
-              className="bg-zinc-700 rounded-sm py-2 px-4  mt-4 text-base text-white"
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            {!validatePassword(password) ? (
-              <label className="text-red-400  text-sm py-1">
-                Minimum length should be 8 characters
-              </label>
-            ) : null}
-            <button
-              type="submit"
-              onSubmit={(e) => e.preventDefault()}
-              onClick={(e) => singIn(e)}
-              className="bg-red-600 hover:bg-red-700 rounded-sm py-1 px-2 mt-10 text-white font-bold text-lg "
-            >
-              {isLoading && <Loader />} Sign In
-            </button>
-            <Link to="/home" className="text-white text-center pt-4">
-              Login as guest
-            </Link>
-            <div className="flex-row flex pt-10">
-              <div className="text-zinc-500">
-                New To Netflix Clone?
-                <Link to="/signup" className="text-white">
-                  {" "}
-                  Sign up now
-                </Link>
+    <div
+      alt="banner"
+      className="flex flex-col w-screen h-screen justify-center items-center"
+    >
+      <img
+        alt="banner"
+        className="w-screen h-screen absolute opacity-40 object-cover"
+        src="https://img.wallpapersafari.com/desktop/1680/1050/24/74/zgeTuV.jpg"
+      />
+      <form className="absolute">
+        <div className="flex justify-center items-center h-screen">
+          <div className="flex-col flex rounded-sm">
+            <div className="flex-col w-96 flex bg-zinc-900 p-10 rounded-sm opacity-90">
+              <img
+                alt="netflix logo"
+                src={NETFLIX_URL}
+                className="w-60 h-36 self-center"
+              ></img>
+              <div className="text-white font-extrabold text-2xl">Sign In</div>
+              <input
+                className="bg-zinc-700 rounded-sm py-2 px-4  mt-6 text-base text-white "
+                type="email"
+                inputMode="email"
+                placeholder="Email or phone number"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              {!validateEmail(username) ? (
+                <label className="text-red-400  text-sm py-1">
+                  Enter a valid email or phone number
+                </label>
+              ) : null}
+              <input
+                className="bg-zinc-700 rounded-sm py-2 px-4  mt-4 text-base text-white"
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              {!validatePassword(password) ? (
+                <label className="text-red-400  text-sm py-1">
+                  Minimum length should be 8 characters
+                </label>
+              ) : null}
+              <button
+                type="submit"
+                onSubmit={(e) => e.preventDefault()}
+                onClick={(e) => singIn(e)}
+                className="bg-red-600 hover:bg-red-700 rounded-sm py-1 px-2 mt-10 text-white font-bold text-lg "
+              >
+                {isLoading && <Loader />} Sign In
+              </button>
+              <Link to="/home" className="text-white text-center pt-4">
+                Login as guest
+              </Link>
+              <div className="flex-row flex pt-10">
+                <div className="text-zinc-500">
+                  New To Netflix Clone?
+                  <Link to="/signup" className="text-white">
+                    {" "}
+                    Sign up now
+                  </Link>
+                </div>
               </div>
             </div>
+            <Footer />
           </div>
-          <Footer />
         </div>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 }
