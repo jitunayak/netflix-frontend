1f7da3749de9be287873f78a3c426e544546ffb4
jitunayak <jitunayak715@gmail.com>
2022-07-06 14:22:09 +0530

    fixed menu getting stick

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
 /**
- * 
- * @param {string[]} param0 
- * @returns 
+ *
+ * @param {string[]} param0
+ * @returns
  */
 export default function Menu({ menuItems }) {
 
@@ -12,7 +12,7 @@ export default function Menu({ menuItems }) {
 
     const navigate = useNavigate();
     return (
-        <div className='fixed flex-col bg-white'>
+        <div className='absolute flex-col bg-white'>
             {menuItems.map((item, index) => {
                 return (
                     <div onClick={() => navigate(`/${item.toLowerCase()}`)} key={index} className='py-2 px-6 text-zinc-900 first:border-t-0 border-t-2  text-sm' >
